3f2d529f1769adba08473a358fbc4303b36b80bb
Diogo-V <diogo.venancio00@gmail.com>
2020-07-24 13:32:50 +0100

    Copied all the files from testing.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello")
+from tkinter import *
+
+
+def myClick():
+    name = "Hello " + entry1.get()
+    myLabel = Label(root, text=name)
+    myLabel.pack()
+
+
+# Creating the window for our application
+root = Tk(className='paws')
+
+entry1 = Entry(root, width=50, fg='Blue', bg='White', borderwidth=5)
+entry1.pack()
+
+
+button1 = Button(root, text="Enter pet name", command=myClick)
+button1.pack()
+
+
+# Creating an event loop. Makes our program run indefinitely
+root.mainloop()
